7baa2185a78276c96e28ac10b97adbb20e63071e
Sarah Marshall <marshallsaraha@gmail.com>
2019-10-14 11:16:18 -0700

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,21 @@ bundle exec jekyll build --destination "$dest" --trace
 
 cd "$dest"
 git add --all
-git commit
-git push
+
+git status
+select choice in "Commit and Push" "Cancel"
+do
+    case $choice in
+        "Commit and Push")
+            git commit -m "Rebuild site"
+            git push
+            break
+            ;;
+        "Cancel")
+            echo "Canceled."
+            break
+            ;;
+    esac
+done
+
 cd -
