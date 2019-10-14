acf892e4fb70d90ead9c676e3ad0cac0077ec7ee
Sarah Marshall <marshallsaraha@gmail.com>
2019-10-14 11:23:36 -0700

    Don't prompt when deploying if no changes made

--- deploy.sh
+++ deploy.sh
@@ -9,20 +9,26 @@ bundle exec jekyll build --destination "$dest" --trace
 cd "$dest"
 git add --all
 
-git status
-select choice in "Commit and Push" "Cancel"
-do
-    case $choice in
-        "Commit and Push")
-            git commit -m "Rebuild site"
-            git push
-            break
-            ;;
-        "Cancel")
-            echo "Canceled."
-            break
-            ;;
-    esac
-done
+if [ -z "$(git status --porcelain)" ]
+then
+    echo "No changes made."
+else
+    git status
+    
+    select choice in "Commit and Push" "Cancel"
+    do
+        case $choice in
+            "Commit and Push")
+                git commit -m "Rebuild site"
+                git push
+                break
+                ;;
+            "Cancel")
+                echo "Canceled."
+                break
+                ;;
+        esac
+    done
+fi
 
 cd -
