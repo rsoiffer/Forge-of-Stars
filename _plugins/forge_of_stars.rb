16eb47a511b3f1318e34b105570d67a4f2065569
Sarah Marshall <marshallsaraha@gmail.com>
2019-11-03 22:43:27 -0800

    Add hover descriptions to weapon special

--- _plugins/forge_of_stars.rb
+++ _plugins/forge_of_stars.rb
@@ -24,16 +24,34 @@ module ForgeOfStars
     def combine_weapons(joined_weapons)
       joined_weapons.flat_map do |type|
         type["shapes"].map do |shape|
+          special =
+            (type["special"] + shape["special"]).sort.uniq.map do |trait|
+              words = trait.split(" ")
+              if /[0-9]/.match?(words.last)
+                {"name" => words[0..-2].join(" "), "value" => words.last}
+              else
+                {"name" => trait}
+              end
+            end
+          
           {
             "category" => shape["category"],
-            "Name" => "#{type["name"]} #{shape["name"]}".capitalize,
-            "Cost" => ("#{(type["cost"] * shape["cost"]).round} cred" if type["cost"] && shape["cost"]),
-            "Damage" => ("#{1 + type["damage_mul"].to_i + shape["damage_mul"].to_i}d#{type["damage"] + shape["damage"]} #{type["damage_type"]}" if type["damage"] && shape["damage"]),
-            "Range" => ("#{(type["range"] * shape["range"]).round} ft" if type["range"] && shape["range"]),
-            "Radius" => ("#{(type["radius"] * shape["radius"]).round} ft" if type["radius"] && shape["radius"]),
-            "Weight" => ("#{(type["weight"] * shape["weight"]).round} lb" if type["weight"] && shape["weight"]),
-            "Hands" => shape["hands"],
-            "Special" => (type["special"] + shape["special"]).sort.uniq.join(", ").capitalize
+            "name" => "#{type["name"]} #{shape["name"]}".capitalize,
+            "cost" => type.key?("cost") && shape.key?("cost") ?
+                      (type["cost"] * shape["cost"]).round : nil,
+            "damage_mul" =>
+              1 + type["damage_mul"].to_i + shape["damage_mul"].to_i,
+            "damage" => type.key?("damage") && shape.key?("damage") ?
+                        type["damage"] + shape["damage"] : nil,
+            "damage_type" => type["damage_type"],
+            "range" => type.key?("range") && shape.key?("range") ?
+                       (type["range"] * shape["range"]).round : nil,
+            "radius" => type.key?("radius") && shape.key?("radius") ?
+                        (type["radius"] * shape["radius"]).round : nil,
+            "weight" => type.key?("weight") && shape.key?("weight") ?
+                        (type["weight"] * shape["weight"]).round : nil,
+            "hands" => shape["hands"],
+            "special" => special
           }
         end
       end
